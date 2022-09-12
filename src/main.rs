dea55a1145432139b9afd6213183a1b28c7a707c
Melanie Haymart <melstromh99@gmail.com>
2022-09-11 23:31:11 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
+use std::{collections::{HashMap, hash_map::{Entry}}, io::{Error, ErrorKind}};
+
 use tide::{Response, Body, Request};
 use dotenv;
-use async_std::{prelude::*, stream::Interval};
+use async_std::{prelude::*};
 use tide_websockets::{Message, WebSocket, WebSocketConnection};
 use tokio::time::{self, Duration};
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> Result<(), tide::Error> {
     dotenv::dotenv().ok();
     tide::log::start();
 
-    
+    let inter = time::interval(Duration::from_millis(1000));
+    let state = State::new();
+    let overseer = Overseer::new(state.clone());
 
-    let mut app = tide::with_state(State::new());
+    tokio::spawn(async move {
+        overseer.start_game(inter).await;
+    }).await.unwrap();
+
+    let mut app = tide::with_state(state);
+
+
+
     app.at("/")
     .get(|_| async move {
         let mut res = Response::new(201);
@@ -22,10 +33,11 @@ async fn main() -> Result<(), std::io::Error> {
     app.at("/ws")
         .get(WebSocket::new(|request: Request<State>, mut stream| async move {
 
-            let inter = time::interval(Duration::from_millis(1000));
+            let state = request.state().clone();
+            let mut client = Client::new(stream.clone());
+            client.id = request.query().unwrap_or_default();
 
-            let overseer = Overseer::new(stream.clone());
-            overseer.start_game(inter).await?;
+            state.add_player_to_game(client.id.to_owned(), client).await?;
 
             while let Some(Ok(Message::Text(input))) = stream.next().await {
                 let output: String = input.chars().rev().collect();
@@ -52,39 +64,71 @@ async fn main() -> Result<(), std::io::Error> {
 
 #[derive(Clone)]
 struct State {
-    message: String,
+    clients: HashMap<String, Client>,
 }
 
 impl State {
     fn new() -> Self {
         Self {
-            message: "blah".to_string(),
+            clients: Default::default(),
         }
     }
+
+    async fn add_player_to_game(&self, client_id: String, client: Client) -> Result<String, std::io::Error> {
+        let mut clients = self.clients.clone();
+        match clients.entry(client_id.to_owned()){
+            Entry::Vacant(_) =>
+            {
+                clients.insert(client_id.to_owned(), client);
+                
+                Ok(String::from("new client added"))
+            },
+            Entry::Occupied(_) =>
+            {
+                return Err(Error::new(ErrorKind::NotConnected, format!("client {} already connected", client_id)));
+            }
+        }
+    }
+
+    async fn send_ticks(&self) ->  Result<(), tide::Error> {
+        for (id, client) in &self.clients {
+            client.wsc.send_string(id.to_string()).await?;
+        }
+        Ok(())
+    }
+    
 }
 
 #[derive(Clone)]
 struct Client {
-    id: Option<String>, // connection Id
+    id: String, // connection Id
     wsc : WebSocketConnection,
-    label : String
+}
+
+impl Client{
+    fn new(stream : WebSocketConnection) -> Self {
+        Self {
+            id: Default::default(),
+            wsc: stream,
+        }
+    }
 }
 
 #[derive(Clone)]
 struct Overseer {
-    connection: WebSocketConnection,
+    state: State,
 }
 
 impl Overseer {
-    fn new(wsc: WebSocketConnection) -> Self {
+    fn new(state: State) -> Self {
         Self {
-            connection: wsc,
+            state: state,
         }
     }
 
-    async fn start_game(&self, mut interval: time::Interval) ->tide::Result<()> {
+    async fn start_game(&self, mut interval: time::Interval) -> Result<(), tide::Error>  {
         loop {
-            self.connection.send_string("this is a message".to_string()).await?;
+            self.state.send_ticks().await?;
             interval.tick().await;
         }
     }
